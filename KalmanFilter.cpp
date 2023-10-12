6379ac427ced64b75d692122a6bd4162985aae07
DangerousCoffee <a.k.dremov@gmail.com>
2023-10-12 19:27:17 +0300

    Added gating distance function

--- KalmanFilter.cpp
+++ KalmanFilter.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b95d5223451c24b93ec7fb2cba7d9f35b5ecddc6aadd38b7412ef19a21feabd
-size 6804
+oid sha256:2711a58609f2f5b07a0587b64851748c96d5f989261532ad74c849fe92a0e3bc
+size 10262
--- KalmanFilterMain.cpp
+++ KalmanFilterMain.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f748ebbacb7601c3c1b9b8734845f076ba2c618834ba59ef73ef3fe8e486014
-size 1122
+oid sha256:f0e68beea57d3452459a3880d4e3207fb219e3257a03d299136d4c189e5e379e
+size 1609
