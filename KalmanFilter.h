4ed31572fe1d37c36b2531be003dae6ab84814c4
DangerousCoffee <a.k.dremov@gmail.com>
2023-10-12 23:58:18 +0300

    pushing leftover files

---
+++ KalmanFilter.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "Eigen/Cholesky"
+#include "iostream"
+#include "map"
+
+using Eigen::Matrix;
+
+#define num_dim 4
+#define double_dim num_dim*2
+#define dt 1.0
+
+class KalmanFilter {
+public:
+	KalmanFilter();
+	virtual ~KalmanFilter();
+	virtual std::pair<Eigen::Vector<float, double_dim>, Eigen::Matrix<float, double_dim, double_dim>> initiate(Eigen::Vector<float, num_dim>);
+	virtual std::pair<Eigen::Vector<float, double_dim>, Eigen::Matrix<float, double_dim, double_dim>> predict(Eigen::Vector<float, double_dim>, Eigen::Matrix<float, double_dim, double_dim>);
+	virtual std::pair<Eigen::Vector<float, num_dim>, Eigen::Matrix<float, num_dim, num_dim>> project(Eigen::Vector<float, double_dim>, Eigen::Matrix<float, double_dim, double_dim>);
+	virtual std::pair<Eigen::Vector<float, double_dim>, Eigen::Matrix<float, double_dim, double_dim>> update(Eigen::Vector<float, double_dim>, Eigen::Matrix<float, double_dim, double_dim>, Eigen::Vector<float, num_dim>);
+	virtual Eigen::VectorXf gating_distance(Eigen::Vector<float, double_dim>, Eigen::Matrix<float, double_dim, double_dim>, Eigen::VectorX<Eigen::Vector<float, num_dim>>, bool only_position=false);
+
+private:
+	Eigen::Matrix<float, double_dim, double_dim> motion_mat;
+	Eigen::Matrix<float, num_dim, double_dim> update_mat;
+
+	const float std_weight_pos = 1. / 20;
+	const float std_weight_vel = 1. / 160;
+	const std::map<int, float> chi2magicnumbers = 
+	{
+		{1, 3.8415},
+		{2, 5.9915},
+		{3, 7.8147},
+		{4, 9.4877},
+		{5, 11.070},
+		{6, 12.592},
+		{7, 14.067},
+		{8, 15.507},
+		{9, 16.919}
+	};
+
+	virtual Eigen::VectorXf gating_solve(Eigen::Vector<float, num_dim>, Eigen::Matrix<float, num_dim, num_dim>, Eigen::VectorX<Eigen::Vector<float, num_dim>>);
+	virtual Eigen::VectorXf gating_solve(Eigen::Vector2f, Eigen::Matrix2f, Eigen::VectorX<Eigen::Vector2f>);
+
+	virtual Eigen::MatrixX<float> nested_vector_to_matrix(Eigen::VectorX<Eigen::Vector<float, num_dim>>);
+	virtual Eigen::MatrixX<float> nested_vector_to_matrix(Eigen::VectorX<Eigen::Vector2f>);
+};
---
+++ KalmanFilterMain.cpp
@@ -0,0 +1,31 @@
+// KalmanFilter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+#include "KalmanFilter.h"
+
+int main()
+{
+    KalmanFilter filter = KalmanFilter();
+    Eigen::Matrix<float, double_dim, double_dim> cov = Eigen::Matrix<float, double_dim, double_dim>::Ones();
+    Eigen::VectorX<Eigen::Vector<float, num_dim>> measurements(2);
+
+    measurements << Eigen::Vector<float, num_dim>(2, 4, 6, 8), Eigen::Vector<float, num_dim>(3, 5, 7, 9);
+
+    filter.initiate(Eigen::Vector<float, num_dim>(2, 4, 6, 8));
+    filter.predict(Eigen::Vector<float, double_dim>(1, 2, 3, 4, 5, 6, 7, 8), cov);
+    filter.project(Eigen::Vector<float, double_dim>(1, 2, 3, 4, 5, 6, 7, 8), cov);
+    filter.update(Eigen::Vector<float, double_dim>(1, 2, 3, 4, 5, 6, 7, 8), cov, Eigen::Vector<float, num_dim>(2, 4, 6, 8));
+    filter.gating_distance(Eigen::Vector<float, double_dim>(1, 2, 3, 4, 5, 6, 7, 8), cov, measurements, true);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
